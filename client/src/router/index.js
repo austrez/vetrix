1d5a9823e66094fe84530a88d3db4ca4ec296645
austrez <austrez@yandex.com>
2022-04-14 23:08:28 +0300

    setup basic vue-router;  add app views

---
+++ client/src/router/index.js
@@ -0,0 +1,32 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import TodoView from '@/views/TodoView.vue';
+import TimerView from '@/views/TimerView.vue';
+import NotesView from '@/views/NotesView.vue';
+import LoginView from '@/views/LoginView.vue';
+
+const routes = [
+	{
+		path: '/todo',
+		component: TodoView,
+	},
+	{
+		path: '/timer',
+		component: TimerView,
+	},
+	{
+		path: '/notes',
+		component: NotesView,
+	},
+	{
+		path: '/login',
+		component: LoginView,
+	},
+];
+
+const router = createRouter({
+	history: createWebHistory(),
+	routes,
+});
+
+export default router;
---
+++ client/src/views/TodoView.vue
@@ -0,0 +1,9 @@
+<template>
+	<h1>This is Todo</h1>
+</template>
+
+<script>
+export default {
+	setup() {},
+};
+</script>

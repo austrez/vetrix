c116abe8adf85955f0c99e745e398c1b6919ec9d
austrez <austrez@yandex.com>
2022-05-02 14:39:21 +0500

    change BaseSvg name to AppSvg; globally register AppSvg

--- client/src/main.js
+++ client/src/main.js
@@ -1,9 +1,12 @@
 import { createApp } from 'vue';
 
 import App from '@/App.vue';
+import AppSvg from '@/components/AppSvg.vue';
 import router from '@/router/index.js';
 
 const app = createApp(App);
+
+app.component('AppSvg', AppSvg);
 app.use(router);
 
 app.mount('#app');

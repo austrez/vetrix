efdf22d2dacddcbe9797014e44c33886f7cd3ec7
austrez <austrez@yandex.com>
2022-05-06 19:43:12 +0500

    storybook: register AppSvg globally; import common styles.scss to preview.js

--- client/.storybook/preview.js
+++ client/.storybook/preview.js
@@ -1,9 +1,15 @@
+import { app } from '@storybook/vue3';
+import AppSvg from '@/components/AppSvg.vue';
+import '@/stories/styles/styles.scss';
+
+app.component('AppSvg', AppSvg);
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
